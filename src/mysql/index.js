b4e25644ac809f766cd84ab2306c6338cd8c2cfc
“jianglang” <lang.jiang@ximalaya.com>
2021-08-03 12:09:39 +0800

    feat: 测试提交

--- src/mysql/index.js
+++ src/mysql/index.js
@@ -1,23 +1,26 @@
-var mysql = require('mysql')
+var mysql = require("mysql");
 var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '511871',
-  port: '3306',
-  database: 'myblog',
-})
+  host: "localhost",
+  user: "root",
+  password: "511871",
+  port: "3306",
+  database: "myblog",
+});
+
 // 开始连接
-con.connect()
+con.connect();
+
 // 执行sql语句
-var sql = 'select * from users;'
+var sql = "select * from users;";
 con.query(sql, function (err, result) {
   if (err) {
-    return console.log(err)
+    return console.log(err);
   }
-  console.log(result)
-  return result
-})
-con.end()
+  console.log(result);
+  return result;
+});
+
+con.end();
 
 // { Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client at Handshake.Sequence._packetToError
 // 解决办法：（修改加密规则为普通模式，默认是严格加密模式）
